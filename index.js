69b7c5aa20d129d7a1cc5437a9416e4766c68183
Sajid Saleem Khattak <sajidsaleem707@gmail.com>
2025-05-28 16:03:12 +0500

    will be working this time

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
-app.use(cors());
-
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
